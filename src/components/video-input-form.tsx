11a40349327e1d2c502ee70552aa99f28ef3781d
Lubnnia <lubnnia@gmail.com>
2023-09-17 15:31:59 -0300

    refactor: Update video-input-form component

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,9 +3,21 @@ import { Separator } from './ui/separator';
 import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react';
+import { getFFmpeg } from '@/lib/ffmpeg';
+import { fetchFile } from '@ffmpeg/util';
 
 function VideoInputForm() {
+  // Variável, função para alterar o valor da variável (estado)
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+
+  // Dentro do estado é onde iremos armazenar o vídeo selecionado
+  // Estado é aquela variável, onde queremos monitorar a troca de valor dela, ou seja, se com
+  // base em uma determinada variável queremos modificar alguma coisa na interface da
+  // aplicação, então, essa variável é um estado
+
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
+
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget;
 
@@ -14,13 +26,84 @@ function VideoInputForm() {
     }
 
     const selectedFile = files[0];
+    // Quando executamos o comando "currentTarget", sempre será retornado um array de
+    // arquivos, porém só queremos pegar um arquivo, por isso colocamos files[0]
+
+    setVideoFile(selectedFile);
   }
 
+  async function convertVideoToAudio(video: File) {
+    console.log('Convert started');
+
+    const ffmpeg = await getFFmpeg();
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video));
+
+    // ffmpeg.on('log', (log) => {
+    //   console.log(log);
+    // });
+
+    ffmpeg.on('progress', (progress) => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100));
+    });
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3',
+    ]);
+
+    const data = await ffmpeg.readFile('output.mp3');
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' });
+    const audioFile = new File([audioFileBlob], 'audio.mp3', {
+      type: 'audio/mpeg',
+    });
+
+    console.log('Convert finished');
+
+    return audioFile;
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault(); // Comando para evitar que a tela seja recarregada
+
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) {
+      return;
+    }
+
+    // converter o vídeo em aúdio
+
+    const audioFile = await convertVideoToAudio(videoFile);
+
+    console.log(audioFile, prompt);
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) {
+      return null;
+    }
+
+    return URL.createObjectURL(videoFile); // Criando uma URL de pré visualização do arquivo.
+  }, [videoFile]);
+
+  // A variável previewURL vai ser recriada novamente somente se a variável "videoFile" mudar,
+  // por isso a mesma é passada no array de dependências, pois estamos monitorando a mesma
+
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleUploadVideo} className="space-y-6">
       <label
         htmlFor="video"
         className="
+          relative
           border 
           flex 
           rounded-md 
@@ -35,8 +118,18 @@ function VideoInputForm() {
           text-muted-foreground
           hover:bg-primary/5"
       >
-        <FileVideo className="w-4 h-4" />
-        Selecione um vídeo
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
       </label>
 
       <input
@@ -53,6 +146,7 @@ function VideoInputForm() {
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
 
         <Textarea
+          ref={promptInputRef}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras chaves mencionadas no vídeo separadas por vírgula (,)"
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import path from 'path'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
